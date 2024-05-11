9dede5374d756cb810974725923fdacb3381a22a
Anna <boginskajaanna@gmail.com>
2024-05-11 21:59:14 +0300

    Created react app and first component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header className="header">
+        <img src="https://www.offidocs.com/images/logobirdgreen.jpg" alt="logo" />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,29 @@
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1468434453985-b1ca3b555f00?ixlib=rb-4.0.3"
+            alt="main img"
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.vexels.com/media/users/3/140236/isolated/preview/20d26c1064c9a7a4ccfc85820529b3f1-female-profile-avatar-3.png"
+            alt="avatar"
+          />
+          <div>descr</div>
+        </div>
+        <div>
+          My posts
+          <div>New post</div>
+        </div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
